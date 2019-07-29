30eec59407f87aea7fc98b8edfdf3095b415fd55
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-29 17:37:04 +0800

    Create getCookie function in cookie util

--- utils/cookie.js
+++ utils/cookie.js
@@ -8,3 +8,23 @@ export const setCookie = (key, value) => {
     });
   }
 };
+
+const getCookieFromBrowser = key => cookie.get(key);
+
+const getCookieFromServer = (key, req) => {
+  if (!req.headers.cookie) {
+    return undefined;
+  }
+  const rawCookie = req.headers.cookie
+    .split(';')
+    .find(c => c.trim().startsWith(`${key}=`));
+  if (!rawCookie) {
+    return undefined;
+  }
+  return rawCookie.split('=')[1];
+};
+
+export const getCookie = (key, req) => (
+  process.browser
+    ? getCookieFromBrowser(key)
+    : getCookieFromServer(key, req));

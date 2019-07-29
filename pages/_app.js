c221948a827021c89f6bf240a7f224ff9ac902e3
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-29 14:23:10 +0800

    Set up _app.js in pages

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import { Provider } from 'react-redux';
+import App, { Container } from 'next/app';
+import withRedux from 'next-redux-wrapper';
+import configureStore from '../redux/configureStore';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+    return pageProps;
+  }
+
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
+}
+
+export default withRedux(configureStore)(MyApp);

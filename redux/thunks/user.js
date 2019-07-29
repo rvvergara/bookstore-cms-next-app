3bf46a146ae888f959307c418c940d3a52f75827
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-29 18:40:17 +0800

    Remove references to localStorage

--- redux/thunks/user.js
+++ redux/thunks/user.js
@@ -5,7 +5,6 @@ import { setCookie } from '../../utils/cookie';
 
 const setUserInStore = (user, dispatch) => {
   const { token } = user;
-  // localStorage.setItem('token', token);
   setCookie('token', token);
   setAuthorizationToken(token);
   dispatch(setCurrentUser({ authenticated: true, data: user }));
@@ -14,7 +13,6 @@ const setUserInStore = (user, dispatch) => {
 
 const setErrorInStore = (err, dispatch) => {
   dispatch(setErrors(err.response.data));
-  // localStorage.clear();
   dispatch(setCurrentUser({ authenticated: false, data: null }));
 };
 

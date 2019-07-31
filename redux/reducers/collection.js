5bf4d525b45aae3a0b4eb6314d04a4483a993b82
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-31 17:19:11 +0800

    Add removeItem to collection reducer

--- redux/reducers/collection.js
+++ redux/reducers/collection.js
@@ -1,9 +1,11 @@
-import { SET_COLLECTION } from '../actions/actionTypes';
+import { SET_COLLECTION, REMOVE_ITEM } from '../actions/actionTypes';
 
 export default (state = [], action) => {
   switch (action.type) {
     case SET_COLLECTION:
       return action.collection;
+    case REMOVE_ITEM:
+      return state.filter(item => item.id !== action.id);
     default:
       return state;
   }

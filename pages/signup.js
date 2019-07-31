2777ee45cb39fc6b550d8508ce56345855bc4c49
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-07-31 11:24:31 +0800

    Set errors to null during loading of signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,10 +1,15 @@
 import Layout from '../components/Layout';
 import UserForm from '../components/UserForm';
+import { setErrors } from '../redux/actions/errors';
 
 export const SignUp = () => (
   <Layout title="Sign Up">
     <UserForm />
   </Layout>
 );
+
+SignUp.getInitialProps = (ctx) => {
+  ctx.store.dispatch(setErrors(null));
+};
 
 export default SignUp;

fed61ca43fe93f304946142f7b7236cd014f2f04
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-01 14:29:47 +0800

    Add search related constants

--- redux/actions/actionTypes.js
+++ redux/actions/actionTypes.js
@@ -1,4 +1,6 @@
+export const LIST_SEARCH_RESULTS = 'LIST_SEARCH_RESULTS';
 export const REMOVE_ITEM = 'REMOVE_ITEM';
 export const SET_COLLECTION = 'SET_COLLECTION';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
 export const SET_ERRORS = 'SET_ERRORS';
+export const SET_SEARCH_TERM = 'SET_SEARCH_TERM';

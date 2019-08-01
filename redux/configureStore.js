ed4927eaafb37701bd9b47d5e434b4acf3f8443c
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-01 19:25:50 +0800

    Fix middleware issue

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,15 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-const middleWares = composeWithDevTools(applyMiddleware(thunk, logger));
-
-const devTools = process.env.NODE_ENV === 'development' ? middleWares : undefined;
+const middleWares = process.env.NODE_ENV === 'development' ? composeWithDevTools(applyMiddleware(thunk, logger)) : compose(applyMiddleware(thunk));
 
 export default (initialState = {}) => createStore(
   rootReducer,
   initialState,
-  devTools,
+  middleWares,
 );

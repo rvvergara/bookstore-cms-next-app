dd59c64ceb99e4a9654b8aa4ebe7be13cc802c83
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-05 17:38:31 +0800

    Add fetchBooksFromLibrary library thunk

--- redux/thunks/library.js
+++ redux/thunks/library.js
@@ -1,11 +1,22 @@
 import { fetchData } from '../../utils/api';
 import { setBook } from '../actions/book';
+import { setDisplayedBooks } from '../actions/library';
 
 export const fetchBook = id => async (dispatch) => {
   const path = `/v1/books/${id}`;
-  return await fetchData('get', path)
+  return fetchData('get', path)
     .then((res) => {
       dispatch(setBook(res.data.book));
+      return res.data;
+    });
+};
+
+export const fetchBooksFromLibrary = page => async (dispatch) => {
+  const path = page ? `/v1/books?page=${page}` : '/v1/books';
+
+  return fetchData('get', path)
+    .then((res) => {
+      dispatch(setDisplayedBooks(res.data.books));
       return res.data;
     });
 };

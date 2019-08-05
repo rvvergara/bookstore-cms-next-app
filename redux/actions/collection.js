d7d16cd27227762ef5d014731dec5f553a24c7b5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-05 09:08:40 +0800

    Add switchPageUpdate & updatePage action creators

--- redux/actions/collection.js
+++ redux/actions/collection.js
@@ -1,4 +1,9 @@
-import { SET_COLLECTION, REMOVE_ITEM } from './actionTypes';
+import {
+  REMOVE_ITEM,
+  SET_COLLECTION,
+  SWITCH_PAGE_UPDATE,
+  UPDATE_PAGE,
+} from './actionTypes';
 
 export const setCollection = collection => ({
   type: SET_COLLECTION,
@@ -8,4 +13,15 @@ export const setCollection = collection => ({
 export const removeItem = id => ({
   type: REMOVE_ITEM,
   id,
+});
+
+export const switchPageUpdate = item_id => ({
+  type: SWITCH_PAGE_UPDATE,
+  item_id,
+});
+
+export const updatePage = (item_id, newPage) => ({
+  type: UPDATE_PAGE,
+  item_id,
+  newPage,
 });

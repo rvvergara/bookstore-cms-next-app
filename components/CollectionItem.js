d3957ee9ce9978bacee6ec116b971b586289d339
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-06 10:53:34 +0800

    Refactor and correct CollectionItem
    1. Remove try catch statement
    2. Conditionally show progress update button depending on whether
    router.query.username is same as username of current user

--- components/CollectionItem.js
+++ components/CollectionItem.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Progress from './Progress';
 import CurrentPage from './CurrentPage';
 import { fetchRemoveItem } from '../redux/thunks/collection';
@@ -20,59 +21,57 @@ const CollectionItem = ({
     current_page,
     page_count,
   } = item;
-  try {
-    const { username } = currentUser.data;
-    return (
-      <div className="card">
-        <div className="card-body">
-          <div className="columns">
-            <div className="book-info">
-              <h4 className="genre">{category}</h4>
-              <Link
-                href="/library/[book]"
-                as={`/library/${book_id}`}
+  const router = useRouter();
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="columns">
+          <div className="book-info">
+            <h4 className="genre">{category}</h4>
+            <Link
+              href="/library/[book]"
+              as={`/library/${book_id}`}
+            >
+              <a>
+                <h3 className="title">{title}</h3>
+              </a>
+            </Link>
+            <p>
+            by:&nbsp;
+              {authors}
+            </p>
+            <div className="action">
+              {
+              currentUser.data.id === owner_id && (
+              <button
+                className="btn btn-link"
+                type="button"
+                onClick={() => fetchRemoveItem(username, item_id)}
               >
-                <a>
-                  <h3 className="title">{title}</h3>
-                </a>
-              </Link>
-              <p>
-              by:&nbsp;
-                {authors}
-              </p>
-              <div className="action">
-                {
-                currentUser.data.id === owner_id && (
-                <button
-                  className="btn btn-link"
-                  type="button"
-                  onClick={() => fetchRemoveItem(username, item_id)}
-                >
-                Remove
-                </button>
-                )
-            }
-              </div>
+              Remove
+              </button>
+              )
+          }
             </div>
-            <div className="progress">
-              <Progress
-                currentPage={Number(current_page)}
-                pages={Number(page_count)}
-              />
-            </div>
-            <div className="chapter-info">
-              <CurrentPage currentPage={String(current_page)} />
+          </div>
+          <div className="progress">
+            <Progress
+              currentPage={Number(current_page)}
+              pages={Number(page_count)}
+            />
+          </div>
+          <div className="chapter-info">
+            <CurrentPage currentPage={String(current_page)} />
+            {router.query.username === currentUser.data.username && (
               <div className="progress">
                 <PageUpdateBtn item_id={item_id} />
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
-    );
-  } catch (err) {
-    return <div>ERROR</div>;
-  }
+    </div>
+  );
 };
 
 const mapStateToProps = state => ({

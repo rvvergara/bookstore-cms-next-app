fdc5851811bf7d849845180abbc4c0b409e4987f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-07 17:17:35 +0800

    Transfer googleBookSearch to search thunk file

--- redux/thunks/search.js
+++ redux/thunks/search.js
@@ -1,6 +1,7 @@
 import { fetchData } from '../../utils/api';
 import { listSearchResults } from '../actions/search';
 import { setSearchTerm } from '../actions/searchTerm';
+import { getUnique, sanitizeBooks } from '../../utils/arrayProcessing';
 
 export const searchLibrary = (keyword, page) => async (dispatch) => {
   const path = page ? `/v1/search/books?q=${keyword};page=${page}` : `/v1/search/books?q=${keyword}`;
@@ -11,4 +12,19 @@ export const searchLibrary = (keyword, page) => async (dispatch) => {
       return { books: res.data.books, count: res.data.count };
     })
     .catch(err => console.log(err));
+};
+
+export const googleBookSearch = (keyword, queryPage) => async (dispatch) => {
+  const page = queryPage || 1;
+  const startIndex = (page - 1) * 40;
+  const path = `https://www.googleapis.com/books/v1/volumes?q=${keyword}&maxResults=40&startIndex=${startIndex}&orderBy=relevance`;
+
+  const rawItems = await fetchData('get', path).then(res => res.data.items)
+    .then(books => sanitizeBooks(books))
+    .then(sanitizedBooks => getUnique(sanitizedBooks))
+    .then(uniqueItems => uniqueItems.slice(startIndex, 10))
+    .then(googleBooks => dispatch(listSearchResults(googleBooks)))
+    .catch(err => console.log(err));
+
+  return rawItems;
 };

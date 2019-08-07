6eddadaed77e634c17406caf73f2ee68044767a2
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-07 11:08:25 +0800

    Create admin page

---
+++ pages/admin.js
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux';
+import redirect from 'next-redirect';
+import Link from 'next/link';
+import initialize from '../utils/initialize';
+import Layout from '../components/Layout';
+import { setAuthorizationToken } from '../utils/api';
+
+const AdminPage = () => (
+  <Layout title="Admin">
+    <div>
+      <Link href="/admin/users">
+        <a>Manage Users</a>
+      </Link>
+    </div>
+    <div>
+      <Link href="/admin/library">
+        <a>Manage Library</a>
+      </Link>
+    </div>
+  </Layout>
+);
+
+AdminPage.getInitialProps = async (ctx) => {
+  initialize(ctx);
+  const { store } = ctx;
+  const { currentUser } = store.getState();
+  if (!currentUser.authenticated) {
+    return redirect(ctx, '/login');
+  }
+  try {
+    setAuthorizationToken(currentUser.data.token);
+  } catch (err) {
+    err;
+  }
+  return { };
+};
+export default connect(state => state)(AdminPage);

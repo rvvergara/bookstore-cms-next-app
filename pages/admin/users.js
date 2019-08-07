11ec56093c084cd33044654640ac0aad6931127e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-07 12:17:54 +0800

    Paginate users list results in admin/users page

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -2,22 +2,49 @@ import { connect } from 'react-redux';
 import redirect from 'next-redirect';
 import initialize from '../../utils/initialize';
 import Layout from '../../components/Layout';
+import Pagination from '../../components/Pagination';
 import { setAuthorizationToken } from '../../utils/api';
 import { fetchUsers } from '../../redux/thunks/user';
 
-const UsersPageForAdmin = ({ users, count }) => (
-  <Layout title="Manage Users">
-    <h4>Users List Here</h4>
-    {
+const UsersPageForAdmin = ({ users, count, page }) => {
+  const currentCount = users.length + (page - 1) * 10;
+  const shownCount = currentCount - users.length + 1;
+  const pagesCount = Math.ceil(count / 10);
+  const pageNumbers = [...Array(pagesCount).keys()].map(el => el + 1);
+  return (
+    <Layout title="Manage Users">
+      <h4>
+        Showing
+        {' '}
+        {shownCount}
+        {' '}
+        to
+        {' '}
+        { currentCount }
+        {' '}
+        of
+        {' '}
+        {count}
+        {' '}
+        users
+      </h4>
+      {
       users.map(user => <h5 key={user.id}>{user.first_name}</h5>)
     }
-  </Layout>
-);
+      <Pagination
+        pages={pageNumbers}
+        queryPage={page}
+        path="/admin/users?"
+      />
+    </Layout>
+  );
+};
 
 UsersPageForAdmin.getInitialProps = async (ctx) => {
   initialize(ctx);
   const { store, query } = ctx;
   const { currentUser } = store.getState();
+  const page = Number(query.page) || 1;
   if (!currentUser.authenticated) {
     return redirect(ctx, '/login');
   }
@@ -25,7 +52,7 @@ UsersPageForAdmin.getInitialProps = async (ctx) => {
   let count;
   try {
     setAuthorizationToken(currentUser.data.token);
-    const data = await store.dispatch(fetchUsers(query.page));
+    const data = await store.dispatch(fetchUsers(page));
     users = data.users;
     count = data.count;
   } catch (err) {
@@ -33,7 +60,7 @@ UsersPageForAdmin.getInitialProps = async (ctx) => {
     users = [];
     count = 0;
   }
-  return { users, count };
+  return { users, count, page };
 };
 
 export default connect(state => state)(UsersPageForAdmin);

6f395f169797bd25747ac69e5d1e9599fa966f04
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-07 15:08:03 +0800

    Fix link in UserManagementPanel

--- components/UserManagementPanel.js
+++ components/UserManagementPanel.js
@@ -9,17 +9,20 @@ const UserManagementPanel = ({ user }) => {
   return (
     <tr key={user.id}>
       <td className="name-col">
-        <Link href={`/users/${username}`}>
+        <Link
+          href="/users/[username]"
+          as={`/users/${username}`}
+        >
           <a>
-            {`${user.first_name} ${user.last_name}`}
+            {`${first_name} ${last_name}`}
           </a>
         </Link>
       </td>
       <td className="username-col">
-        {user.username}
+        {username}
       </td>
-      <td className="email-col">{user.email}</td>
-      <td className="admin-col">{user.access_level}</td>
+      <td className="email-col">{email}</td>
+      <td className="admin-col">{access_level}</td>
     </tr>
   );
 };

f526fb59d48a70725022e272d1c5e26afe37067f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-10 13:18:16 +0800

    Populate admin/library new and edit pages

--- pages/admin/library/[book]/edit.js
+++ pages/admin/library/[book]/edit.js
@@ -1,7 +1,33 @@
+import { connect } from 'react-redux';
+import redirect from 'next-redirect';
+import initialize from '../../../../utils/initialize';
+import { setAuthorizationToken } from '../../../../utils/api';
+import { fetchBook } from '../../../../redux/thunks/library';
 import Layout from '../../../../components/Layout';
 
-const EditBookPage = () => (
-  <Layout title="Edit Book"><h3>Edit Book Page</h3></Layout>
+const EditBookPage = ({ book }) => (
+  <Layout title={`Edit - ${book.title}`}><h3>Edit Book Page</h3></Layout>
 );
 
-export default EditBookPage;
+EditBookPage.getInitialProps = async (ctx) => {
+  initialize(ctx);
+  const { store, query } = ctx;
+  const { getState, dispatch } = store;
+  const { currentUser } = getState();
+  if (!currentUser.authenticated) {
+    return redirect(ctx, '/login');
+  }
+  if (currentUser.data.access_level < 2) {
+    return redirect(ctx, '/');
+  }
+  try {
+    const { token } = currentUser.data;
+    setAuthorizationToken(token);
+    await dispatch(fetchBook(query.book));
+  } catch (err) {
+    err;
+  }
+  return { };
+};
+
+export default connect(state => state)(EditBookPage);
--- pages/admin/library/new.js
+++ pages/admin/library/new.js
@@ -1,7 +1,30 @@
+import { connect } from 'react-redux';
+import redirect from 'next-redirect';
+import initialize from '../../../utils/initialize';
+import { setAuthorizationToken } from '../../../utils/api';
 import Layout from '../../../components/Layout';
 
 const NewBookPage = () => (
   <Layout title="Add To Library"><h3>New Book Page</h3></Layout>
 );
 
-export default NewBookPage;
+NewBookPage.getInitialProps = (ctx) => {
+  initialize(ctx);
+  const { store } = ctx;
+  const { getState } = store;
+  const { currentUser } = getState();
+  if (!currentUser.authenticated) {
+    return redirect(ctx, '/login');
+  }
+  if (currentUser.data.access_level < 2) {
+    return redirect(ctx, '/');
+  }
+  try {
+    const { token } = currentUser.data;
+    setAuthorizationToken(token);
+  } catch (err) {
+    err;
+  }
+};
+
+export default connect(state => state)(NewBookPage);

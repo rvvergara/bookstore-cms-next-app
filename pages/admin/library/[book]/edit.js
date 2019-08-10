774bea2b38af45f0bcabde932570dc78a1576e45
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-08-10 12:11:09 +0800

    Create new and edit admin book pages

---
+++ pages/admin/library/[book]/edit.js
@@ -0,0 +1,7 @@
+import Layout from '../../../../components/Layout';
+
+const EditBookPage = () => (
+  <Layout title="Edit Book"><h3>Edit Book Page</h3></Layout>
+);
+
+export default EditBookPage;
---
+++ pages/admin/library/new.js
@@ -0,0 +1,7 @@
+import Layout from '../../../components/Layout';
+
+const NewBookPage = () => (
+  <Layout title="Add To Library"><h3>New Book Page</h3></Layout>
+);
+
+export default NewBookPage;
